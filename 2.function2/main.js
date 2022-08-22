af6120bbe1ffe95c06cb5cfffad02495eadb4030
Marcelo Magday <marcelo.angelo.magday@oocl.com>
2022-08-22 15:18:20 +0800

    Did JS-Function

---
+++ 2.function2/main.js
@@ -0,0 +1,17 @@
+// - Create a new main.js file and write a function that does the following: determine whether a string is a palindrome string. (Palindrome, a string read from the beginning as well as from the end. For example, abcba is a palindrome string.)
+
+// ```
+// function palindrome(message){
+//   // wirte your code here
+// }
+// palindrome('hello'); // should return false
+// palindrome('abcba'); // should return true
+// ```
+
+function palindrome(message) {
+    let reverse = message.split("").reverse().join("");
+    return message == reverse;
+}
+
+palindrome('hello');
+palindrome('abcba');
---
+++ 3.function3/main.js
@@ -0,0 +1,16 @@
+// ## requirement 
+    
+// - Create a new main.js file and write a function that outputs the incoming argument string alphabetically.
+
+// ```
+// function alphabetSort(message){
+//   // wirte your code here
+// }
+// alphabetSort('hello'); // should return 'ehllo'
+// ```
+
+function alphabetSort(message) {
+    return message.split('').sort().join('');
+}
+
+alphabetSort('hello');
